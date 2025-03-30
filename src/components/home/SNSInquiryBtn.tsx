c9609a023692447993a8d57fa25c3ca3f0e4d20e
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-03-30 18:18:31 +0900

    🍀 refactor: 문의하기 정보들 constant로 분리

--- src/components/home/SNSInquiryBtn.tsx
+++ src/components/home/SNSInquiryBtn.tsx
@@ -1,41 +1,18 @@
 import { useState } from 'react';
 import SpaceBtn from '../../assets/imgs/Spacebtn.svg';
-import Mail from '../../assets/imgs/Mail.svg';
-import Instagram from '../../assets/imgs/Instagram.svg';
-import KakaoTalk from '../../assets/imgs/KakaoTalk.svg';
 import CloseBtn from '../../assets/imgs/CloseBtn.svg';
 import PlusBtn from '../../assets/imgs/PlusBtn.svg';
 import BlackIconBtn from './BlackIconBtn';
-
-type ContactType = 'email' | 'instagram' | 'kakao';
-
-const contactLabels: Record<ContactType, string> = {
-  email: '이메일 문의',
-  instagram: '인스타그램 문의',
-  kakao: '카카오톡 문의',
-};
-
-const contactIcons: Record<ContactType, string> = {
-  email: Mail,
-  instagram: Instagram,
-  kakao: KakaoTalk,
-};
-
-// TODO: email 링크 추후 변경
-const contactLinks: Record<ContactType, string> = {
-  email: 'https://www.konkuk-kuit.com/5/introduce',
-  instagram: 'https://www.instagram.com/kuit.official/',
-  kakao: 'http://pf.kakao.com/_Dxbgvxj',
-};
+import { CONTACTS } from '../../constants/ContactConstants';
 
 const SNSInquiryBtn = () => {
   const [open, setOpen] = useState(false);
-  const [activeType, setActiveType] = useState<ContactType | null>(null);
+  const [activeType, setActiveType] = useState<string | null>(null);
 
-  const handleToggle = (type: ContactType) => {
+  const handleToggle = (type: string, link: string) => {
     if (activeType === type) {
       // 같은 버튼 두 번째 클릭 시 링크 이동
-      window.location.href = contactLinks[type];
+      window.location.href = link;
     } else {
       setActiveType(type); // 새로운 버튼 열기
     }
@@ -47,14 +24,14 @@ const SNSInquiryBtn = () => {
 
       {open && (
         <div className="flex flex-col items-end gap-24">
-          {(['email', 'instagram', 'kakao'] as ContactType[]).map((type) => (
+          {CONTACTS.map(({ type, label, icon, link }) => (
             <BlackIconBtn
               key={type}
-              src={contactIcons[type]}
-              label={contactLabels[type]}
-              href={contactLinks[type]}
+              src={icon}
+              label={label}
+              href={link}
               isActive={activeType === type}
-              onToggle={() => handleToggle(type)}
+              onToggle={() => handleToggle(type, link)}
             />
           ))}
         </div>
