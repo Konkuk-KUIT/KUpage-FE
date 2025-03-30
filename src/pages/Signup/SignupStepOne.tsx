6654957b56674f4ccbd446164c9845a2ab39f2e6
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-03-30 21:31:01 +0900

    feat: Signup Step 분리

---
+++ src/pages/Signup/SignupStepOne.tsx
@@ -0,0 +1,16 @@
+import SignupInputBox from '../../components/signup/SignupInputBox';
+
+interface SignupStepOneProps {
+  name: string;
+  onChange: (value: string) => void;
+}
+
+const SignupStepOne = ({ name, onChange }: SignupStepOneProps) => {
+  return (
+    <div className="ml-80 mr-40">
+      <SignupInputBox label="이름" value={name} onChange={onChange} placeholder="ex) 홍길동" />
+    </div>
+  );
+};
+
+export default SignupStepOne;
