8859041fcf479ec9fe0b34a34f1929c3fff92823
kim jihwan <kjhwan0802@naver.com>
2025-03-31 23:20:21 +0900

    feat: Routing & dummy Header, Footer

---
+++ src/pages/Global/Layout.tsx
@@ -0,0 +1,30 @@
+import { JSX, Suspense } from 'react';
+import { matchPath, Outlet, useLocation } from 'react-router-dom';
+
+import Footer from './Footer';
+import Header from './Header';
+
+interface RoutesChildren {
+  routes_children: {
+    path: string;
+    element: JSX.Element;
+    hasHeader?: boolean;
+    hasFooter?: boolean;
+  }[];
+}
+
+const Layout = ({ routes_children }: RoutesChildren) => {
+  const { pathname } = useLocation();
+
+  return (
+    <>
+      {routes_children.find((child) => matchPath(child.path, pathname))?.hasHeader && <Header />}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+      {routes_children.find((child) => matchPath(child.path, pathname))?.hasFooter && <Footer />}
+    </>
+  );
+};
+
+export default Layout;
