1dd281f1e4775615976ec4b4ae9d82a692802d56
sooa@naver.com <leesooa0316@naver.com>
2025-05-01 17:34:49 +0900

    Fix: pr6 내용 추가

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,18 @@
+import { ENDPOINTS } from '../config';
+import useApi from './useApi';
+
+const useLogin = () => {
+  const { api } = useApi();
+
+  const login = () => {
+    api.post(ENDPOINTS.MEMBERS, {});
+  };
+
+  const authLogin = () => {
+    api.post(ENDPOINTS.AUTH, {});
+  };
+
+  return { login, authLogin };
+};
+
+export default useLogin;
