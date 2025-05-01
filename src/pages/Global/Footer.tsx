df8258082ca099b042928918c001f55ad8d5c4e8
sooa@naver.com <leesooa0316@naver.com>
2025-05-01 17:26:37 +0900

    Revert "Merge pull request #6 from Konkuk-KUIT/feat/api-setting"
    This reverts commit 90dcf6a3a9a0d5f022f20de35e952f41bfef6c91, reversing
    changes made to 7c8c0c60e683e904d7a34231944dd19d155a53f6.

---
+++ src/pages/Global/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <p>&copy; 2023 Your Company. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
