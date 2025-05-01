6bf50f2685ad12fc11f386e498b5d4abe556c719
sooa@naver.com <leesooa0316@naver.com>
2025-05-01 17:26:37 +0900

    Revert "Merge pull request #6 from Konkuk-KUIT/feat/api-setting"
    This reverts commit f68faf614fa8b25ba3347e8966f1384f3fe562a4, reversing
    changes made to 6421c02829d78e54844067ed546efe634b89e992.

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
-
-export const ENDPOINTS = {
-  MEMBERS: '/members',
-  AUTH: '/auth',
-} as const;
-
-export type EndpointKey = keyof typeof ENDPOINTS;
-export type EndpointValue = (typeof ENDPOINTS)[EndpointKey];
