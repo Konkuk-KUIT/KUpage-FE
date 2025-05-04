2ee96790c8ccc8bb04192920f966ddcba124fa72
kim jihwan <kjhwan0802@naver.com>
2025-05-04 21:25:44 +0900

    style: update Login component layout and adjust Tailwind CSS breakpoints

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,23 +3,25 @@ import TextButton from '../../components/commons/TextButton';
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-between bg-signup-gradient  min-h-screen w-screen px-120">
-      <img src={BigKUITLogo} alt="KUIT Logo" className="w-[55rem] " />
-      <div className="text-left justify-between">
+    <div className="flex items-center justify-between bg-signup-gradient min-h-screen w-full px-40 md:px-120 overflow-x-hidden">
+      <div className="hidden md:block">
+        <img src={BigKUITLogo} alt="KUIT Logo" className="min-w-[55rem]" />
+      </div>
+      <div className="text-left justify-between mx-auto md:mx-0">
         <div className="h-[40rem]">
-          <h1 className="text-80 font-800  ">
+          <h1 className="text-48 md:text-80 font-800">
             <span className="text-main">KU </span>
             <span className="text-white">CAN DO</span>
             <span className="text-main"> IT</span>
           </h1>
-          <p className="text-32 font-500 mt-12">
+          <p className="text-24 md:text-32 font-500 mt-12">
             <span className="text-white">건국대학교 기획/개발 동아리</span> <br />
             <span className="text-main font-600">KUIT</span>
             <span className="text-white">에 오신 걸 환영합니다.</span>
           </p>
         </div>
         <div className="mt-32">
-          <TextButton text="디스코드로 로그인하기" px="px-160" py="py-20" />
+          <TextButton text="디스코드로 로그인하기" px="px-80 md:px-160" py="py-16 md:py-20" />
         </div>
       </div>
     </div>
