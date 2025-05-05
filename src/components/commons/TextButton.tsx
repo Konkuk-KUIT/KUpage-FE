c1485c3dcbdddbc311a7e3f0a8b9a7b5e1f3e981
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2025-05-05 13:32:02 +0900

    Design: Calendar 크기 수정

--- src/components/commons/TextButton.tsx
+++ src/components/commons/TextButton.tsx
@@ -10,7 +10,7 @@ const TextButton = ({
   text,
   onClick,
   px = 'px-12',
-  py = 'py-6',
+  py = 'py-8',
   isActive = false,
 }: TextButtonProps) => {
   const baseClasses = `text-24 font-700 rounded-8 whitespace-nowrap transition-colors duration-200 ${px} ${py} cursor-pointer border`;
