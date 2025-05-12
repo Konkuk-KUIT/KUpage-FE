f02c75e1e17c0cba2814c03403263628364a341f
Turtle-Hwan <kjhwan0802@naver.com>
2025-05-12 22:04:21 +0900

    feat: "거쳐간 사람들" 추가, InprogressBox 확장성 개선

--- src/components/home/InProgressBox.tsx
+++ src/components/home/InProgressBox.tsx
@@ -3,15 +3,16 @@ import Counter from './Counter';
 interface InProgressBoxProps {
   title: string;
   count: number;
+  counters?: string;
 }
 
-const InProgressBox = ({ title, count }: InProgressBoxProps) => {
+const InProgressBox = ({ title, count, counters = '개' }: InProgressBoxProps) => {
   return (
     <div className="flex-center flex-col">
       <p className="text-white font-500 text-24">{title}</p>
-      <div className="mt-20 flex items-end px-104 py-40 rounded-10 border-border border-2 gap-4">
+      <div className="mt-20 flex justify-center items-center w-252 h-152 rounded-10 border-border border-2 gap-4">
         <Counter end={count} duration={2000} />
-        <span className="text-white font-700 text-20 mb-12">개</span>
+        <span className="text-white font-700 text-20 mt-12">{counters}</span>
       </div>
     </div>
   );
