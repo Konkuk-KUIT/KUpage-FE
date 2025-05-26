04d4a24a4adc69e80fa316728c0102a615dbf7d5
kim jihwan <kjhwan0802@naver.com>
2025-05-26 17:31:45 +0900

    🧹 refactor: enhance route type definitions

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,35 @@
+import React, { JSX } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Creators from './pages/Creators/Creators';
 import ErrorPage from './pages/Global/ErrorPage';
 import Layout from './pages/Global/Layout';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
 import Projects from './pages/Project/Projects';
 import Recruit from './pages/Recruit/Recruit';
-import Creators from './pages/Creators/Creators';
+import Signup from './pages/Signup/Signup';
+
+type RouteChild = {
+  path: string;
+  element: JSX.Element;
+  hasHeader?: boolean;
+  hasFooter?: boolean;
+};
 
 const Router = () => {
-  const routes_children_auth = [
+  const routes_children_auth: RouteChild[] = [
     { path: '/login', element: <Login />, hasHeader: true, hasFooter: true },
     { path: '/signup', element: <Signup />, hasHeader: true, hasFooter: true },
   ];
 
-  const routes_children = [
+  const routes_children: RouteChild[] = [
     { path: '/', element: <Home />, hasHeader: true, hasFooter: true },
     { path: '/recruit', element: <Recruit />, hasHeader: true, hasFooter: true },
     { path: '/creators', element: <Creators />, hasHeader: true, hasFooter: true },
+    { path: '/projects', element: <Projects />, hasHeader: true, hasFooter: true },
     ...routes_children_auth,
     { path: '/*', element: <ErrorPage /> },
-    { path: '/projects', element: <Projects />, hasHeader: true },
   ];
 
   const routes = [
