185f51b0ea8b6e944ed5ce025ccecf86ec9e3e99
sooa@naver.com <leesooa0316@naver.com>
2025-05-26 21:43:59 +0900

    Feat: Pagination 적용

--- src/pages/Article/Articles.tsx
+++ src/pages/Article/Articles.tsx
@@ -2,19 +2,28 @@ import { useState } from 'react';
 import ProjectCohortTabs from '../../components/Project/ProjectCohortTabs';
 import SNSInquiryBtn from '../../components/home/SNSInquiryBtn';
 import ArticleCard from '../../components/Article/ArticleCard';
+import Pagination from '../../components/commons/Pagination';
+
+const PAGE_SIZE = 16;
 
 const Articles = () => {
   const [isSelected, setIsSelected] = useState('All');
+  const [pageIdx, setPageIdx] = useState(1);
+
+  const selectArticle = articles.slice(
+    (pageIdx - 1) * PAGE_SIZE,
+    (pageIdx - 1) * PAGE_SIZE + PAGE_SIZE
+  );
   return (
     <div className="relative">
-      <div className="flex flex-col gap-52 bg-gray min-h-screen w-screen px-76 py-100">
+      <div className="flex flex-col items-center gap-52 bg-gray min-h-screen w-screen px-76 py-100">
         <div className="flex flex-col justify-center items-center gap-20">
           <h3 className="text-48 font-700 text-white">KUITLog</h3>
           <span className="text-24 font-500 text-main">다양한 IT 관련 소식을 확인해보세요.</span>
           <ProjectCohortTabs isSelected={isSelected} setIsSelected={setIsSelected} />
         </div>
         <div className="grid grid-rows-4 grid-cols-4 gap-32">
-          {articles.map((article) => (
+          {selectArticle.map((article) => (
             <ArticleCard
               key={article.id}
               title={article.title}
@@ -23,6 +32,7 @@ const Articles = () => {
             />
           ))}
         </div>
+        <Pagination length={Math.ceil(articles.length / PAGE_SIZE)} setPageIdx={setPageIdx} />
       </div>
       <button
         className="px-[45px] py-[20px] rounded-[10px] border border-solid border-border 
@@ -131,6 +141,12 @@ const articles = [
   },
   {
     id: 16,
+    title: '프롬프트 엔지니어링으로 메뉴 이미지 품질 검수하기: GPT 기반 업무 자동화',
+    userName: '김태정',
+    createdAt: '2025-05-01T12:00:00Z',
+  },
+  {
+    id: 17,
     title: '프롬프트 엔지니어링으로 메뉴 이미지 품질 검수하기: GPT 기반 업무 자동화',
     userName: '김태정',
     createdAt: '2025-05-01T12:00:00Z',
