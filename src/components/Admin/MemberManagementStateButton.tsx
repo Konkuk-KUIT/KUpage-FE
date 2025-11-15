ad166dd2346649aa3f86c71106d17d4d38c961f2
sooooa16 <156293624+sooooa16@users.noreply.github.com>
2025-11-16 04:50:51 +0900

    ui(MemberManagementStateButton): 인원 관리 상태 변경하는 버튼 구현

---
+++ src/components/Admin/MemberManagementStateButton.tsx
@@ -0,0 +1,23 @@
+interface MemberManagementStateButtonProps {
+  type: '대기' | '승인' | '거부';
+}
+
+const typeClass = {
+  대기: 'bg-lightGray',
+  승인: 'bg-main',
+  거부: 'bg-[#FF5252]',
+} as const;
+
+const MemberManagementStateButton = ({ type = '대기' }: MemberManagementStateButtonProps) => {
+  return (
+    <button
+      type="button"
+      className="flex flex-center w-156 h-56 border-2 border-border bg-gray rounded-[6px] gap-8"
+    >
+      <div className={`w-16 h-16 rounded-full ${typeClass[type]}`}></div>
+      <span className="text-white text-20 font-700">{type}</span>
+    </button>
+  );
+};
+
+export default MemberManagementStateButton;
