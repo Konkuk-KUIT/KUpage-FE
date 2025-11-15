09a908b790f26b03592482a093038d8307424b96
sooooa16 <156293624+sooooa16@users.noreply.github.com>
2025-11-16 04:49:50 +0900

    ui(MemberManagementChip): 인원 관리 페이지에 사용되는 칩 구현

---
+++ src/components/Admin/MemberManagementChip.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface MemberManagementChipProps {
+  size?: 'medium' | 'small';
+  type: '대기' | '승인' | '거부';
+  value: number;
+}
+
+const sizeClass = {
+  medium: 'w-44 h-40',
+  small: 'w-36 h-32',
+} as const;
+
+const typeClass = {
+  대기: 'bg-lightGray',
+  승인: 'bg-main',
+  거부: 'bg-[#FF5252]',
+} as const;
+
+const MemberManagementChip = ({ size = 'medium', type, value }: MemberManagementChipProps) => {
+  return (
+    <div
+      className={`flex flex-center rounded-[3px] text-gray text-20 font-700 ${sizeClass[size]} ${typeClass[type]}`}
+    >
+      {value}
+    </div>
+  );
+};
+
+export default MemberManagementChip;
