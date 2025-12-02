19346f4cdceca95d11de2b6d8dc7b7b732d6f3d8
Chris Ahn <chrisahn1007@gmail.com>
2025-12-02 19:55:57 +0900

    hotfix: mt주기

--- src/components/TeamMatching/ApplyState/ApplicantModal.tsx
+++ src/components/TeamMatching/ApplyState/ApplicantModal.tsx
@@ -13,7 +13,7 @@ const ApplicantModal = ({ onClose, applicantData }: Props) => {
       onClick={onClose}
     >
       <div
-        className="flex flex-col gap-40 items-end w-808 h-auto px-52 py-40 rounded-20 z-10 bg-gradient"
+        className="flex flex-col gap-40 items-end w-808 h-auto px-52 py-40 rounded-20 z-10 bg-gradient mt-24"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="w-full flex flex-col items-start gap-24">
